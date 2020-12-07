221f097a5e6df671159f750e630594a686110a23
qkfns <qkfns@naver.com>
2020-12-07 13:21:54 +0900

    테스트

---
+++ 연습문제A.sql
@@ -0,0 +1,231 @@
+CREATE TABLE Books (
+                       bookid      INTEGER,
+                       bookname    VARCHAR(40),
+                       publisher   VARCHAR(40),
+                       price       INTEGER
+);
+
+CREATE TABLE  BookMembers (
+                              custid      INTEGER PRIMARY KEY,
+                              name        VARCHAR(40),
+                              address     VARCHAR(50),
+                              phone       VARCHAR(20)
+);
+
+CREATE TABLE BookOrders (
+                            orderid INTEGER,
+                            custid  INTEGER ,
+                            bookid  INTEGER ,
+                            saleprice INTEGER ,
+                            orderdate DATE
+);
+
+INSERT INTO Books VALUES
+(1, '축구의 역사', '굿스포츠', 7000)
+                       ,(2, '축구아는 여자', '나무수', 13000)
+                       ,(3, '축구의 이해', '대한미디어', 22000)
+                       ,(4, '골프 바이블', '대한미디어', 35000)
+                       ,(5, '피겨 교본', '굿스포츠', 8000)
+                       ,(6, '역도 단계별기술', '굿스포츠', 6000)
+                       ,(7, '야구의 추억', '이상미디어', 20000)
+                       ,(8, '야구를 부탁해', '이상미디어', 13000)
+                       ,(9, '올림픽 이야기', '삼성당', 7500)
+                       ,(10, 'Olympic Champions', 'Pearson', 13000);
+
+INSERT INTO BookMembers VALUES
+(1, '박지성', '영국 맨체스타', '000-5000-0001')
+                             ,(2, '김연아', '대한민국 서울', '000-6000-0001')
+                             ,(3, '장미란', '대한민국 강원도', '000-7000-0001')
+                             ,(4, '추신수', '미국 클리블랜드', '000-8000-0001')
+                             ,(5, '박세리', '대한민국 대전',  NULL);
+
+INSERT INTO BookOrders VALUES
+(1, 1, 1, 6000, STR_TO_DATE('2014-07-01','%Y-%m-%d'))
+                            ,(2, 1, 3, 21000, STR_TO_DATE('2014-07-03','%Y-%m-%d'))
+                            ,(3, 2, 5, 8000, STR_TO_DATE('2014-07-03','%Y-%m-%d'))
+                            ,(4, 3, 6, 6000, STR_TO_DATE('2014-07-04','%Y-%m-%d'))
+                            ,(5, 4, 7, 20000, STR_TO_DATE('2014-07-05','%Y-%m-%d'))
+                            ,(6, 1, 2, 12000, STR_TO_DATE('2014-07-07','%Y-%m-%d'))
+                            ,(7, 4, 8, 13000, STR_TO_DATE( '2014-07-07','%Y-%m-%d'))
+                            ,(8, 3, 10, 12000, STR_TO_DATE('2014-07-08','%Y-%m-%d'))
+                            ,(9, 2, 10, 7000, STR_TO_DATE('2014-07-09','%Y-%m-%d'))
+                            ,(10, 3, 8, 13000, STR_TO_DATE('2014-07-10','%Y-%m-%d'));
+
+
+
+
+-- 연습문제A1
+-- 1) 모든 도서의 이름과 가격을 조회하세요
+select bookname ,price
+from Books ;
+
+-- 1b) 모든 도서의 가격,이름을 조회하세요
+select price, bookname
+from Books;
+
+-- 2) 모든 도서의 번호, 이름, 출판사, 가격을 조회하세요
+select bookid,bookname,publisher,price
+from Books;
+-- 2b) 모든 도서의 정보를 조회하세요
+select * from Books;
+
+-- 3) 모든 도서의 출판사를 조회하세요
+select publisher
+from Books;
+-- 3b) 모든 도서의 출판사를 조회하세요 (중복제거)
+select DISTINCT publisher
+from Books;
+-- 4) 고객의 이름, 전화번호를 조회하세요 단, 출력결과의 필드명을 '이름,전화번호'로 바꿔 출력
+
+SELECT  name '이름', phone '전화번호'
+FROM BookMembers;
+
+-- 5) 고객의 이름과 핸드폰번호를 조회하세요
+select name, phone
+from BookMembers;
+
+-- 6) 가격이 20000미만인 도서를 조회하세요
+select *
+from Books
+where price < 20000;
+
+-- 7) 가격이 15000이상 ~25000이하인 도서의 제목을 조회하세요
+select bookname
+from Books
+where price BETWEEN 15000 and 25000;
+
+-- 8) 책이름이 '축구의 역사' 또는 '피겨 교본'인 도서의 출판사를 조회하세요
+select publisher as 출판사
+FROM Books
+where bookname = '축구의 역사' or bookname = '피겨 교본';
+
+select bookname, publisher
+from Books
+where bookname in ('축구의 역사', '피겨 교본');
+
+-- 9) '축구'로 시작하는 도서 이름을 조회하세요.
+select bookname
+from Books
+where bookname like '축구%';
+
+-- 9b) '축구'로 끝나는 도서 이름을 조회하세요
+select bookname
+from Books
+where bookname like '%축구';
+
+-- 9c) '축구'를 포함하는 도서 이름을 조회하세요
+select bookname
+from Books
+where bookname like '%축구%';
+
+-- 9d) 도서이름 중에 '의'를 포함하는 책은?
+
+select bookname
+from Books
+where bookname like '%의%';
+
+-- 10) '골프'를 포함하고 가격이 30000이상인 도서 이름을 조회하세요
+
+select bookname
+from Books
+where bookname like '%골프%' and price >= 30000;
+
+-- 11) 고객이름을 가나다순으로 오름차순 정렬하고
+-- 이름이 같으면 주소로 내림차순 정렬해서 조회하세요
+
+select name , address
+from BookMembers
+order by name asc , address desc;
+
+-- 11b) 도서의 가격을 내림차순으로,
+-- 가격이 같으면 출판사이름으로 오름차순 정렬해서 조회하세요
+
+select price , publisher
+from Books
+order by price desc , publisher asc;
+
+-- 12) 고객이 주문한 도서의 총, 평균, 최고,
+-- 최저 판매액을 조회하세요
+
+select sum(saleprice) '총 판매액', avg(saleprice) 평균, max(saleprice) 최고, min(saleprice) 최저
+from BookOrders;
+
+-- 13b) 출판사별 도서의 총 수량 조회하세요
+
+select publisher ,count(bookid)
+from Books
+group by publisher
+
+--  14) 출판사별 도서의 총 수량이 2권이상인 출판사 조회하세요
+
+select publisher , count(bookid)
+from Books
+group by publisher
+HAVING count(bookid)>=2;
+
+--  14b) 가격이 8000이상인 도서를 구매한 고객별
+--  주문 도서의 총 수량을 구하세요
+
+select custid, count(bookid)
+from BookOrders
+where saleprice >= 8000
+group by custid;
+
+
+--  14b) 가격이 8000이상인 도서를 구매한 고객별
+--  주문 도서의 총 수량을 구하세요
+--  단, 고객별 2권이상 주문한 고객을 대상으로 한다
+
+select custid, count(bookid)
+from BookOrders bo
+where saleprice >= 8000
+group by custid
+having count(bookid)>=2;
+
+--  15) 주문한 고객번호,고객이름과 주문금액,주소를 조회하세요
+-- 고객 테이블과 주문테이블을 결합해야 조회 가능
+-- 즉, 외래키 관꼐에 있는 컬럼을 대상으로 결합
+
+--  15b) 박지성 고객의 주문금액,주소,주문일자를 조회하세요
+
+
+--  16) 주문한 도서이름,주문금액,주문일자를 조회하세요
+
+
+--  19) 가격이 20000인 도서를 주문한 고객 이름 조회하세요
+
+
+--  20) 주문한 고객이름, 도서이름, 주문일자 출력하세요
+
+
+
+--  21) 도서를 구매하지 않은 고객을 포함하여
+--     고객이름과 주문한 도서의 판매금액을 조회하세요
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 연습문제B.sql
@@ -0,0 +1,196 @@
+-- 연습문제B1
+-- 1. 모든 사원의 이름 (FIRST_NAME, LAST_NAME)을 조회하라.
+select first_name,last_name
+from EMPLOYEES;
+
+-- 2. 모든 사원의 모든 정보를 조회하라.
+select * from EMPLOYEES;
+
+-- 3. 모든 도시명을 조회하라.
+select city
+from LOCATIONS;
+
+-- 4. 이름(FIRST_NAME)이 M으로 시작하는 사원의 모든 정보를 조회하라.
+select *
+from EMPLOYEES
+where first_name like 'M%';
+
+-- 5. 이름(FIRST_NAME)의 두 번째 글자가 'a'인 사원의 이름(FIRST_NAME)과 연봉을 조회하라.
+
+select first_name '사원의 이름', salary 연봉
+from EMPLOYEES
+where first_name like '_a%';
+
+-- 6. 도시 명을 오름차순으로 정렬하라.
+
+select city
+from LOCATIONS
+order by city asc;
+
+-- 7. 부서 명을 내림차순 정렬하라.
+
+select department_name
+from DEPARTMENTS
+order by department_name desc;
+
+-- 8. 연봉이 7000 이상인 사원들의 모든 정보를 연봉순(오름차순)으로 정렬하라.
+
+select *
+from EMPLOYEES
+where salary >=7000
+order by salary asc;
+
+-- 9. 인센티브(COMMISSION_PCT)를 받지 않는 사원들의 모든 정보를 조회하라.
+
+select *
+from EMPLOYEES
+where commission_pct is NULL ;
+
+-- 10. 인센티브(COMMISSION_PCT)를 받는 사원들의 모든 정보를 조회하라.
+
+select *
+from EMPLOYEES
+where commission_pct is not NULL;
+
+-- 11. 2007년 06월 21일에 입사한 사원의 사원번호, 이름(FIRST_NAME, LAST_NAME) 그리고 부서번호를 조회하라.
+
+select employee_id 사원번호, first_name , last_name , department_id
+from EMPLOYEES
+where hire_date ='2007-06-21';
+
+-- 12. 2006년에 입사한 사원의 사원번호와 입사일을 조회하라.
+
+select employee_id 사원번호, hire_date 입사일
+from EMPLOYEES e2
+where hire_date like '%2006%'
+order by hire_date;
+
+-- 13. 이름(FIRST_NAME)의 길이가 5글자 이상인 사원을 조회하라.
+
+select *
+from EMPLOYEES
+where first_name like '_____%'
+order by first_name asc;
+
+-- 14. 연봉이 5000 에서 12000의 범위 이외인 사람들의 LAST_NAME 및 연봉을 조회힌다.
+
+select last_name , salary
+from EMPLOYEES
+where salary < 5000 or salary >12000;
+
+-- 15. 사원번호가 176 인 사람의 LAST_NAME 과 부서 번호를 조회한다.
+
+select last_name , department_id '부서 번호'
+from EMPLOYEES
+where employee_id = 176;
+
+-- 16. 연봉이 12000 이상되는 직원들의 LAST_NAME 및 연봉을 조회한다.
+
+select last_name , salary
+from EMPLOYEES
+where salary >= 12000
+order by salary asc;
+
+-- 17. 1994년도에 고용된 모든 사람들의 LAST_NAME 및 고용일을 조회한다.
+
+select last_name , hire_date
+from EMPLOYEES
+where hire_date like '%1994%';
+
+-- 18. 매니저가 없는 사람들의 LAST_NAME 및 JOB_ID 를 조회한다.
+
+select last_name , job_id
+from EMPLOYEES
+where manager_id is NULL;
+
+-- 19. 커미션을 버는 모든 사원들의 LAST_NAME, 연봉 및 커미션을 조회한다.
+--     단, 연봉 역순, 커미션 역순으로 정렬한다.
+
+select last_name ,salary , commission_pct
+from EMPLOYEES
+where commission_pct is not null
+order by salary desc,commission_pct desc;
+
+-- 20. LAST_NAME 의 네번째 글자가 a 인 사원들의 LAST_NAME 을 조회한다.
+
+select last_name
+from EMPLOYEES
+where last_name like '___a%';
+
+-- 21. LAST_NAME 에 a 및(OR) e 글자가 있는 사원들의 LAST_NAME 을 조회한다.
+
+select last_name
+from EMPLOYEES
+where last_name like '%a%' or last_name like '%e%';
+
+-- 22. 연봉이 2,500, 3,500, 7000 이 아니며 직업이 SA_REP 이나 ST_CLERK 인 사람들을 조회한다.
+
+select *
+from EMPLOYEES
+where (salary <> 2500 and salary <> 3500 and salary <> 7000) and (job_id = 'SA_REP' or job_id = 'ST_CLERK');
+
+-- 23. 회사 전체의 최대 연봉, 최소 연봉, 연봉 총 합 및 평균 연봉을 자연수로 포맷하여 조회한다.
+
+select max(salary) '최대 연봉', min(salary) '최소 연봉', sum(salary) '연봉 총합', ROUND(AVG(salary)) '평균 연봉'
+from EMPLOYEES;
+
+-- 24. 각 JOB_ID 별, 최대 연봉, 최소 연봉, 연봉 총 합 및 평균 연봉을 자연수로 포맷하여 조회한다.
+
+select job_id, max(salary)'최대 연봉' , min(salary)'최소 연봉', sum(salary) '연봉 총 합', ROUND(AVG(salary)) '평균 연봉'
+from EMPLOYEES
+group by job_id;
+
+-- 25. 동일한 직업을 가진 사원들의 총 수를 조회한다.
+
+select job_id, count(employee_id) '직업군별 사원 총수'
+from EMPLOYEES
+group by job_id;
+
+-- 26. 매니저로 근무하는 사원들의 총 수를 조회한다.
+
+select count(DISTINCT(manager_id)) '매니저로 근무하는 사원 수'
+from EMPLOYEES;
+
+-- 27. 사내의 최대 연봉 및 최소 연봉의 차이를 조회한다.
+
+select max(salary) - min(salary) as '최고 최소 연봉 차이'
+from EMPLOYEES;
+
+-- 28. 매니저의 사번 및 그 매니저 밑 사원들 중 최소 연봉을 받는 사원의 연봉을 조회한다.
+--     매니저가 없는 사람들은 제외한다.
+--     최소 연봉이 6000 미만인 경우는 제외한다.
+--     연봉 기준 역순으로 조회한다.
+
+select employee_id 사원번호, manager_id '담당 매니저 번호',min(salary) '연봉'
+from EMPLOYEES
+where salary >= 6000
+group by manager_id
+having manager_id is not null
+order by min(salary) desc;
+
+-- 29. 부서 명, 위치 ID, 각 부서 별 사원 총 수, 각 부서 별 평균 연봉을 조회한다.
+--     평균 연봉은 소수점 2 자리까지만 표현한다.
+
+select department_id , COUNT(employee_id) ,ROUND(AVG(salary),2)
+from EMPLOYEES
+group by department_id;
+
+
+-- 30. 총 사원 수 및 2005, 2006, 2007, 2008 년도 별 고용된 사원들의 총 수를 다음과 같이 조회한다.
+
+
+
+-- 31. 부서번호별 사원수를 조회하라. (부서번호 오름차순 정렬)
+
+select department_id 부서번호, count (employee_id) '부서번호별 사원수'
+from EMPLOYEES
+group by department_id
+having department_id is not null
+order by department_id asc;
+
+-- 32.  각 부서별 각 직업 별 연봉 총 합 및 평균 연봉을 조회한다.
+
+select department_id '부서번호', job_id '직업',sum(salary) '연봉 총 합', round(avg(salary)) '평균 연봉'
+from EMPLOYEES
+group by department_id , job_id
+having department_id is not null;
